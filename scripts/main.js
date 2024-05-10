b05913f88ef6866a64d4fc0290d4127facb152a7
kamalnanda21 <kamalnanda01@gmail.com>
2024-05-10 14:08:45 +0530

    sec 6,7 responsive done,slider done

--- scripts/main.js
+++ scripts/main.js
@@ -16,22 +16,33 @@ hamburger.addEventListener("click", function() {
 
 // slick slider
 
-$(document).ready(function(){
-  $('.slide').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      dots: false,
-      infinite: true,
-      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-      nextArrow: '<button type="button" class="slick-next">Next</button>',
-      responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 1
-              }
+$('.slick-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000, // Autoplay speed in milliseconds
+  arrows: true, // Show arrows
+  prevArrow: '<button type="button" class="slick-prev"></button>', // Custom prev arrow
+  nextArrow: '<button type="button" class="slick-next"></button>', // Custom next arrow
+  responsive: [
+      {
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
           }
-      ]
-  });
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+  ]
 });
