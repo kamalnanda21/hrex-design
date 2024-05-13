0218c98725fbfcb617a957fb36971c8cf39d006e
kamalnanda21 <kamalnanda01@gmail.com>
2024-05-13 20:00:06 +0530

    sec1,2,5 responsive done plus gradient bg feature sec

--- scripts/feature.js
+++ scripts/feature.js
@@ -30,3 +30,18 @@ $('.card-area').slick({
         }
     ]
   });
+
+// hamburger menu 
+
+const hamburger = document.getElementById("hamburger");
+const mobileNavbar = document.getElementById("mobile-navbar");
+
+hamburger.addEventListener("click", function() {
+  mobileNavbar.classList.toggle("open");
+  hamburger.classList.toggle("cross");
+  if (mobileNavbar.classList.contains("open")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+});
